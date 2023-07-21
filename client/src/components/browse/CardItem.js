2f10b58d8709330d0dc535de2de9ff2af39c1721
Levi Barsis <lbarsis@gmail.com>
2023-07-21 14:14:21 -0700

    saved  buttons to card items

--- client/src/components/browse/CardItem.js
+++ client/src/components/browse/CardItem.js
@@ -5,8 +5,8 @@ function CardItem({ card }) {
   return (
     <div className='card-item'>
       <button className='card-button'>Add to Collection</button>
-      <button className='card-button'>Add to Collection</button>
-      <button className='card-button'>Add to Collection</button>
+      <button className='card-button'>Add to Deck</button>
+      <button className='card-button'>Add to Wishlist</button>
       {
         card.image_uris ? <img src={card?.image_uris?.normal} alt='cardimage' /> : <img src={card?.card_faces[0]?.image_uris?.normal} alt='cardimage' />
       }
