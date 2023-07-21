04fd625ec21f189ccdd81a81a0e27eb0a93850ce
Levi Barsis <lbarsis@gmail.com>
2023-07-21 14:13:52 -0700

    added buttons to card items

--- client/src/components/browse/CardItem.js
+++ client/src/components/browse/CardItem.js
@@ -4,6 +4,9 @@ import '../../styles/browse/cardItem.css'
 function CardItem({ card }) {
   return (
     <div className='card-item'>
+      <button className='card-button'>Add to Collection</button>
+      <button className='card-button'>Add to Collection</button>
+      <button className='card-button'>Add to Collection</button>
       {
         card.image_uris ? <img src={card?.image_uris?.normal} alt='cardimage' /> : <img src={card?.card_faces[0]?.image_uris?.normal} alt='cardimage' />
       }
