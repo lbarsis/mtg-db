45a9370d9e7ecf298f399acd727910b1e0fcd38f
Levi Barsis <lbarsis@gmail.com>
2023-07-29 14:21:38 -0700

    updated wishlist_cards_controller for adding cards to wishlists

--- app/controllers/wishlist_cards_controller.rb
+++ app/controllers/wishlist_cards_controller.rb
@@ -1,2 +1,31 @@
 class WishlistCardsController < ApplicationController
+  def create
+    wishlist = @current_user.wishlists.find(params[:wishlist_id])
+    card = Card.find_by(card_data: params[:card])
+
+    if card.nil?
+      card = Card.create(card_data: params[:card])
+    end
+
+    wishlist_card = wishlist.wishlist_cards.find_by(card_id: card.id)
+    if wishlist_card
+      wishlist_card.quantity += 1
+      wishlist_card.save!
+    else
+      wishlist_card = wishlist.wishlist_cards.create!(deck_cards_params.merge(card_id: card.id, quantity: 1))
+    end
+
+    if wishlist_card.persisted?
+      render json: wishlist_card, status: :ok
+    else
+      render json: wishlist_card.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def deck_cards_params
+    params.permit(:card, :wishlist_id)
+  end
+  
 end
